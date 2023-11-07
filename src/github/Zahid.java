2dc3e8df17ad72a77876d740f3a9994b0ae8a1d5
sharif24 <azizur15-2124@diu.edu.bd>
2023-11-07 11:47:21 -0800

    github branch concept

--- src/github/Zahid.java
+++ src/github/Zahid.java
@@ -3,5 +3,6 @@ package github;
 public class Zahid {
     public static void main(String[] args) {
         System.out.println("Welcome To zahid");
+        System.out.println("Welcome SQA Class");
     }
 }
